fd0bd49c31cd0d67af6dcde7c3d63fb953352b9b
nhoanh555 <115225755+nhoanh555@users.noreply.github.com>
2023-06-02 00:56:59 +0700

    Add files via upload

---
+++ dothicohuong.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+
+# Khởi tạo đồ thị
+G = nx.DiGraph()
+
+# Thêm các đỉnh vào đồ thị
+dinh = [1, 2, 3, 4, 5]
+G.add_nodes_from(dinh)
+
+# Thêm các cạnh vào đồ thị
+cung = [(1, 2), (1, 3), (2, 4), (4, 5), (3, 5)]
+G.add_edges_from(cung)
+
+# Vẽ đồ thị và hiển thị lên màn hình
+nx.draw(G, with_labels=True)
+plt.show()
